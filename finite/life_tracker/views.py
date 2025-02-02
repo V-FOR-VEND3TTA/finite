9cd93a31a17c8a2e9a5022f71e21b4f0fce65f14
George Hlongwane <georgehlongwane8@gmail.com>
2025-02-02 06:33:23 +0200

    Created a view for teh user dashboard

--- finite/life_tracker/views.py
+++ finite/life_tracker/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+def dashboard(request):
+    profile = Profile.objects.get(user=request.user)
+    weeks_lived = profile.weeks_lived()
+    weeks_remaining = profile.weeks_remaining()
+    life_completion = profile.life_completion_percentage()
+
+    context = {
+        "weeks_lived": weeks_lived,
+        "weeks_remaining": weeks_remaining,
+        "life_completion": round(life_completion, 2),
+        "total_weeks": profile.expected_lifespan * 52
+    }
+    return render(request, "life_tracker/dashboard.html", context)
